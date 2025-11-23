ddbf9755ac86dd0eea374fd66e2b2860f1711600
Kerem Mert <kerem37mert@yahoo.com>
2025-11-23 12:30:25 +0300

    bug fixed

--- backend/k8s.js
+++ backend/k8s.js
@@ -11,6 +11,13 @@ export const kubernetesClient = new k8s.KubeConfig();
 // Minikube configi otomatik olarak yükler
 kubernetesClient.loadFromDefault();
 
+// HTTP kullanan clusterlar için skipTLSVerify ayarını true yap
+for (const cluster of kubernetesClient.clusters) {
+    if (cluster.server && cluster.server.startsWith("http:")) {
+        cluster.skipTLSVerify = true;
+    }
+}
+
 export const k8sApi = kubernetesClient.makeApiClient(k8s.CoreV1Api);
 export const k8sAppsApi = kubernetesClient.makeApiClient(k8s.AppsV1Api);
 export const k8sNetworkingApi = kubernetesClient.makeApiClient(k8s.NetworkingV1Api);

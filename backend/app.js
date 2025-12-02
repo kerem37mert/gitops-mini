c703e5f93d9bcb196fdbc5897e24096fab906ab0
Kerem Mert <kerem37mert@yahoo.com>
2025-12-02 18:40:37 +0300

    bug fixed

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,12 @@ import { startAutoSync } from "./autoSyncScheduler.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
